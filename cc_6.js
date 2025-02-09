62e80354cdd320046e66f5ac9ea20fecd4c84d32
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-09 11:13:04 -0500

    "Task 2 - Sales Tax Computation." beginning

--- cc_6.js
+++ cc_6.js
@@ -4,3 +4,10 @@ function calculateProfit(costPrice, sellingPrice, unitsSold){
 }
 calculateProfit(20,30,100) // data to show total profit 
 calculateProfit(50,70,200) // same with this one (both shoudl return with different outputs)
+
+function calculateSalesTax (amount, taxRate) {
+let Salestaxes = amount * taxRate;
+}
+
+calculateSalesTax(100, 0.07); 
+calculateSalesTax(500, 0.1);  
