07e2bcf761a92fb7ed50d882f4388d848e1e30b8
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-09 11:10:33 -0500

    "Task 1 - Business Profit Calculation."

--- cc_6.js
+++ cc_6.js
@@ -2,5 +2,5 @@ function calculateProfit(costPrice, sellingPrice, unitsSold){
     let profit = (sellingPrice - costPrice) * unitsSold;
     console.log(`Total Profit: $${profit}`);
 }
-calculateProfit(20,30,100)
-calculateProfit(50,70,200)
+calculateProfit(20,30,100) // data to show total profit 
+calculateProfit(50,70,200) // same with this one (both shoudl return with different outputs)
