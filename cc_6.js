bd07ee7921566dbff0e796e2ac75013dfd014cbc
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-09 12:19:33 -0500

    small additions

--- cc_6.js
+++ cc_6.js
@@ -4,7 +4,7 @@ function calculateProfit(costPrice, sellingPrice, unitsSold){
     console.log(`Total Profit: $${profit}`);
 }
 calculateProfit(20,30,100); // data to show total profit 
-calculateProfit(50,70,200);// same with this one (both shoudl return with different outputs)
+calculateProfit(50,70,200);// same with this one (both should return with different outputs)
 
 // "Task 2 - Sales Tax Computation."
 function calculateSalesTax (amount, taxRate) {
@@ -30,8 +30,8 @@ let calculateBonus = (salary, performanceRating) => {
         bonusPercentage = 0.05;
 }
 
-let bonus = salary * bonusPercentage;
-console.log (`Bonus: $${bonus}`);
+    let bonus = salary * bonusPercentage;
+    console.log (`Bonus: $${bonus}`);
 }
 calculateBonus(5000, "Excellent"); // output should be 1000
 calculateBonus(7000, "Good");      // output shoudl be 700
